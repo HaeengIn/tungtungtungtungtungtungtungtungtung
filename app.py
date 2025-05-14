cdabbf75b838111f7b268905044e1f3485ef3f0c
HaeengIn <haeengin@gmail.com>
2025-05-14 17:05:02 +0900

    post

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from datetime import datetime
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template("index.html")
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if request.method == 'POST':
+        username = request.form['username']
+        title = request.form['title']
+        content = request.form['content']
+        created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        conn = sqlite3.connect('db/data.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO posts (username, title, content, created_at) VALUES (?, ?, ?, ?)",
+            (username, title, content, created_at)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect('/')
+    return render_template('post.html')

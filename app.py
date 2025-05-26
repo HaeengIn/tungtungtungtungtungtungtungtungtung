169db5f75045757d2f8650791937eda738ac65f2
HaeengIn <haeengin@gmail.com>
2025-05-26 21:13:19 +0900

    사칭 방지지

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -28,6 +32,11 @@ def write():
         username = request.form['username'].strip()
         if not username:
             username = '교하고 학생'
+        if username == 'TVS Dev Team':
+            request_password = request.form.get('이 닉네임을 사용하려면 비밀번호를 입력하세요.', '')
+            admin_password = os.getenv('admin_password')
+            if request_password != admin_password:
+                return "비밀번호가 일치하지 않습니다.", 403
         title = request.form['title']
         content = request.form['content']
         created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')

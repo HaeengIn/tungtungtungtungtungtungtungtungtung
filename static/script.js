8f772b6d4ef50eb25a2f0a0624942dc882767720
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2025-05-28 04:51:27 +0000

    fix password error

--- static/script.js
+++ static/script.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(e) {
         var username = document.getElementById('username').value.trim();
         if (username === 'TVS Dev Team') {
+            e.preventDefault();
             var request_password = prompt('이 닉네임을 사용하려면 비밀번호를 입력하세요: ');
             if (request_password === null) {
-                e.preventDefault();
                 return false;
             }
             if (request_password === '') {
@@ -14,12 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
                 e.preventDefault();
                 return false;
             }
-            if (request_password !== admin_password) {
-                alert('비밀번호가 일치하지 않습니다.');
-                e.preventDefault();
-                return false;
-            }
-            document.getElementById('admin_password').value = admin_password;
+            document.getElementById('admin_password').value = request_password;
+
+            var password_data = new FormData(form);
+            fetch('write', {
+                method: 'POST',
+                body: password_data
+            })
+            .then(response => response.text())
+            .then(html => {
+                if (html.includes('password_error')) {
+                    alert('비밀번호가 맞지 않습니다.');
+                    e.preventDefault();
+                    return false;
+                } else {
+                    window.location.href = '/';
+                }
+            })
         }
     });
 });

ba9194ccbdba53ae4c5f64a7bccf904430e06621
LChayim <shmoon@anaden.co.kr>
2025-09-23 01:05:46 +0900

    fix route

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import os
 
 load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
@@ -19,12 +21,6 @@ def index(request: Request):
     response = supabase.table("ght_posts").select("*").order("id", desc=True).execute()
     posts = response.data
     return templates.TemplateResponse("index.html", {"request": request, "posts": posts})
-
-@app.get("/{id}")
-def view(request: Request, id: int):
-    response = supabase.table("ght_posts").select("*").eq("id", id).single().execute()
-    posts = response.data
-    return templates.TemplateResponse("view.html", {"request": request, "posts": posts})
 
 @app.get("/write")
 def write(request: Request):
@@ -36,3 +32,9 @@ def writePosts(request: Request, username: str = Form(...), title: str = Form(..
     newPosts = insert.data[0]
     newId = insert.data["id"]
     return RedirectResponse(url="/", status_code=303)
+
+@app.get("/{id}")
+def view(request: Request, id: int):
+    response = supabase.table("ght_posts").select("*").eq("id", id).single().execute()
+    posts = response.data
+    return templates.TemplateResponse("view.html", {"request": request, "posts": posts})

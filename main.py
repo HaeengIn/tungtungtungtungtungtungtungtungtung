b6c9b3a8aeba3f6dcbd60c336a847acaec698303
HaeengIn <haeengin@gmail.com>
2025-09-23 04:57:20 +0000

    fix datetime format

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from supabase import create_client, Client
 from dotenv import load_dotenv
+from datetime import datetime, timezone, timedelta
 import os
 
 load_dotenv()
@@ -16,10 +17,19 @@ templates = Jinja2Templates(directory="templates")
 
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
+def fixDatetime(posts):
+    for post in posts:
+        if "created_at" in post and post["created_at"]:
+            strCreatedAt = post["created_at"]
+            if isinstance(strCreatedAt, str):
+                strCreatedAt = datetime.fromisoformat(strCreatedAt)
+            post["created_at"] = strCreatedAt.strftime("%Y/%m/%d %H/%M/%S")
+    return posts
+
 @app.get("/")
 def index(request: Request):
     response = supabase.table("ght_posts").select("*").order("id", desc=True).execute()
-    posts = response.data
+    posts = fixDatetime(response.data)
     return templates.TemplateResponse("index.html", {"request": request, "posts": posts})
 
 @app.get("/write")
@@ -34,5 +44,5 @@ def writePosts(request: Request, username: str = Form(...), title: str = Form(..
 @app.get("/{id}")
 def view(request: Request, id: int):
     response = supabase.table("ght_posts").select("*").eq("id", id).single().execute()
-    posts = response.data
-    return templates.TemplateResponse("view.html", {"request": request, "posts": posts})
+    posts = fixDatetime([response.data])
+    return templates.TemplateResponse("view.html", {"request": request, "posts": posts[0]})
